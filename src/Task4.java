88e5c6bf7dbb21a34f4e2966ea07df69565c6afb
polgarrr <nikitina.ma@phystech.edu>
2022-02-07 23:16:01 +0300

    Add solution for task 4

---
+++ src/Task4.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Task4 {
+    public static double[] createRandomArray() {
+        double[] randomArray = new double[9]; // создала массив с рандомными значениями
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = Math.random();
+        }
+        return randomArray;
+    }
+    public static void getMinMaxAvg(double[] arr) {
+        double min = arr[0]; // объявила мин, макс и сумму
+        double max = arr[0];
+        double sum = 0;
+
+        for (double v : arr) {
+            sum = sum + v; // sum += arr[i]; – суммирую все значения массива
+            if (v < min) {
+                min = v; // определяю минимальное число сравниванием
+            } else if (v > max) {
+                max = v; // определяю максимальное число сравниванием
+            }
+        }
+        double avg = sum / arr.length; // рассчитываю среднее значение массива
+        System.out.println("min value: " + min); // вывод данных
+        System.out.println("max value: " + max);
+        System.out.println("avg value: " + avg);
+    }
+    public static void main(String[] args) {
+        double[] arr = createRandomArray();
+        System.out.println(Arrays.toString(arr)); // показываю массив
+        getMinMaxAvg(arr); // вызываю рассчеты
+    }
+}
